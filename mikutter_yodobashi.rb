ba37ebceb9263848464a35850024cf0f3f30c580
Akira Ouchi <akkiesoft@marokun.net>
2017-11-26 23:46:15 +0900

    2017年式最新的手遅

--- mikutter_yodobashi.rb
+++ mikutter_yodobashi.rb
@@ -18,13 +18,14 @@ Plugin.create(:mikutter_yodobashi) do
     visible: true,
     role: :timeline
   ) do |opt|
+    m=opt.messages[0]
     msgplus = ""
-    to_name = opt.messages[0].user.idname
+    to_name = m.user.to_s
     if to_name.length <= 8 then
       msgplus += "化妆品、电子游戏、"
     end
     msg = "@" + to_name + " Welcome to ヨォドォバァシィカァメェラ。亲爱的顾客朋友、你们好。衷心欢迎您光临友都八喜。友都八喜是日本著名的大型购物中心。精明商品将近一百万种、数码相机、摄像机、名牌手表、" + msgplus + "名牌箱包等应有尽有。最新的款式、最优惠的价格、最优质的服务。"
-    Service.primary.post(:message => msg, :replyto => opt.messages[0])
+    m.post(:message => msg, :replyto => m)
   end
 
   command(
@@ -34,12 +35,13 @@ Plugin.create(:mikutter_yodobashi) do
     visible: true,
     role: :timeline
   ) do |opt|
+    m=opt.messages[0]
     msgplus = ""
-    to_name = opt.messages[0].user.idname
+    to_name = m.user.to_s
     if to_name.length <= 8 then
       msgplus = "数码相机、名牌手表、"
     end
     msg = "@" + to_name + " Welcome to ヨォドォバァシィカァメェラ。手遅的顾客朋友、你们好。衷心欢迎您光临友都手遅。友都八喜是日本手遅的大型购物中心。手遅商品将近一百万种、" + msgplus + "手遅品、手遅机、电子手遅、名牌手遅等应有尽有。最新的手遅、最优惠的手遅、最优质的手遅。"
-    Service.primary.post(:message => msg, :replyto => opt.messages[0])
+    m.post(:message => msg, :replyto => m)
   end
 end
